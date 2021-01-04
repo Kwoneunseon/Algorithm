a845b9bf6b6c7d7c183eadb99c289fbf31f3a4ab
eunseon <kwon9348@naver.com>
2021-01-05 02:27:08 +0900

    깔끔한 코드 추가

--- Programmers/level2/H-Index.cpp
+++ Programmers/level2/H-Index.cpp
@@ -1,6 +1,5 @@
 #include <vector>
 #include <algorithm>
-#include <iostream>
 using namespace std;
 
 int solution(vector<int> citations){
@@ -27,7 +26,16 @@ int solution(vector<int> citations){
 	return solution;
 }
 
-int main() {
-	cout << solution({3, 0, 6, 1, 5} )<< endl;
-	return 0;
+
+// clean code
+int solution(vector<int>citations) {
+	int answer = 0;
+	sort(citations.begin(), citations.end(), greater<int>());
+	for (int i = 0; i < citations.size(); i++)
+	{
+		if (citations[i] >= (i + 1))
+			answer++;
+	}
+
+	return answer;
 }
