60c258f23b821449b9133ed885647775466e4e3e
eunseon <kwon9348@naver.com>
2021-01-05 03:17:02 +0900

    main함수 없앰

--- Programmers/level2/카펫.cpp
+++ Programmers/level2/카펫.cpp
@@ -7,7 +7,7 @@ using namespace std;
 vector<int> solution(int brown, int yellow) {
 	vector<int>answer;
 	int x, total;
-	for (int i = 1; i < sqrt(yellow); i++)
+	for (int i = 1; i <= sqrt(yellow); i++)
 	{
 		if (yellow % i == 0) {
 			x = yellow / i;
@@ -22,13 +22,4 @@ vector<int> solution(int brown, int yellow) {
 	}
 
 	return answer;
-}
-
-int main() {
-	vector<int> answer = solution(8, 1);
-	for (int i = 0; i < answer.size(); i++)
-	{
-		cout << answer[i] << " ";
-	}
-	return 0;
 }
