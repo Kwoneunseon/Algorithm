265eb43b7dcd08a079847dd0af98c72d7f97f7f4
eunseon <kwon9348@naver.com>
2021-01-07 06:10:53 +0900

    주석 추가

--- Programmers/level2/더 맵게.cpp
+++ Programmers/level2/더 맵게.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int solution(vector<int> scovile, int K) {
 	int answer = 0;
 	priority_queue<int, vector<int>, greater<> >pq(scovile.begin(), scovile.end());
-	
+	//가장 안 매운 음식이 K보다 작으면서 남은 음식은 2개 이상일때
 	while (pq.size() > 1 && pq.top() < K) {
 		int first = pq.top();
 		pq.pop();
