45a431ec2d846e8753b7c7763105232ac5d98b9c
eunseon <kwon9348@naver.com>
2021-01-12 00:45:44 +0900

    다른사람풀이 찾음. 이해 못한 부분 있음

--- Programmers/level2/조이스틱.cpp
+++ Programmers/level2/조이스틱.cpp
@@ -1,7 +1,7 @@
 
 #include <vector>
 #include <string>
-
+#include <algorithm>
 using namespace std;
 
 int solution(string name) {
@@ -30,5 +30,30 @@ int solution(string name) {
 		}
 	}
 
+	return answer;
+}
+
+//다른사람의 풀이
+int LUT[] = { 0,1,2,3,4,5,6,7,8,9,10,11,12,13,12,11,10,9,8,7,6,5,4,3,2,1 };
+//미리 움직여야하는 횟수를 계산한다.
+
+int other_solution(string name) {
+	int answer = 0;
+	for (auto ch : name)
+		answer += LUT[ch - 'A'];
+	int len = name.length();
+	int left_right = len - 1;
+	for (int i = 0; i < len; ++i)
+	{
+		int next_i = i + 1;
+		//A가 아닌 char가 i를 기준으로 어디에 있는지 파악.
+		while (next_i < len && name[next_i] == 'A') {
+			next_i++;
+		}
+		// 이부분이 이해가 잘 안감.(다시)
+		left_right = min(left_right, i + len - next_i + min(i, len - next_i));
+	}
+
+	answer += left_right;
 	return answer;
 }
