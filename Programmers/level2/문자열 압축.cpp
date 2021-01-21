e1eb0a40d49e8bf6fd75b2ec6f40faef4136b6d8
eunseon <kwon9348@naver.com>
2021-01-22 02:21:49 +0900

    솔루션 보고 해결

---
+++ Programmers/level2/문자열 압축.cpp
@@ -0,0 +1,36 @@
+﻿#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string s) {
+	int answer = 0;
+	vector<int> lens;//길이 담을 벡터
+	string temp, result;
+	int count = 1;
+	for (int i = 1; i <= s.length()/2; i++)
+	{
+		result = "";
+		for (int j = 0; j < s.length(); j+=i)
+		{
+			temp = s.substr(j, i);
+			if (i + j > s.length() || temp != s.substr(i + j, i)) {
+				if (count != 1) {
+					result += to_string(count);
+				}
+				result += temp;
+				count = 1;
+			}
+			else {
+				count++;
+			}
+		}
+		lens.push_back(result.length());
+	}
+	if (s.length() == 1)answer = 1;
+	else {
+		answer = *min_element(lens.begin(), lens.end());
+	}
+	return answer;
+}
