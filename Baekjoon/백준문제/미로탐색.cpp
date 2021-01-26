d9a611a218318e843821a71475f45b47418f8067
eunseon <kwon9348@naver.com>
2021-01-27 02:32:18 +0900

    dfs로 사용 -> 실패

--- Baekjoon/백준문제/미로탐색.cpp
+++ Baekjoon/백준문제/미로탐색.cpp
@@ -1,63 +1,61 @@
-﻿//bfs로 문제를 풀었는데
-//dfs를 사용해야 할듯.
+﻿//2178번
+//dfs사용
 #include <iostream>
 #include <vector>
-#include <queue>
 #include <string>
 
 using namespace std;
 
-bool visit[100][100] = { false };
-int dx[] = { 0,1,0,-1 };
-int dy[] = { 1,0,-1,0 };
-int row, col,cnt = 0;
-vector< vector<int> >miro;
+int col, row;
+bool visit[100][100] = { false, };
+vector<vector<int> > miro;
+int dx[] = { 1,0,0,-1 };
+int dy[] = { 0,1,-1,0 };
+int cnt = 1, answer;
 
-int bfs(int a, int b) {
-	int x, y, next_x, next_y;
-	queue<pair<int, int> >q;
-	q.push(make_pair(a, b));
-	visit[a][b] = true;
-	while (!q.empty()) {
-		x = q.front().first;
-		y = q.front().second;
-		cnt++;
-		q.pop();
-		if (x == row - 1 && y == col - 1) {
-			return cnt;
-		}
-			for (int i = 0; i < 4; i++)
-			{
-				next_x = x + dx[i];
-				next_y = y + dy[i];
-				if (next_x < row && next_x >= 0 && next_y < col && next_y >= 0 && !visit[next_x][next_y]) {
-					if (miro[next_x][next_y] == 1) {
-						q.push(make_pair(next_x, next_y));
-					}
-					visit[next_x][next_y] = true;
-				}
+void dfs(int x, int y) {
+	if (visit[x][y])
+		return;
+	if (x == row - 1 && y == col - 1)
+		answer = cnt;
+	visit[x][y] = true;
+	cnt++;
+	int next_x, next_y;
+	bool flag = false;
+	for (int i = 0; i < 4; i++)
+	{
+		next_x = x + dx[i];
+		next_y = y + dy[i];
+		if (next_x >= 0 && next_x < row && next_y >= 0 && next_y < col) {
+			if (miro[next_x][next_y] == 1 && !visit[next_x][next_y]) {
+				flag = true;
+				dfs(next_x, next_y);
 			}
+		}
 	}
-	return cnt;
+	if (!flag) {
+		cnt--;
+	}
 }
 
 int main() {
-	int num;
-	string str_num;
 	cin >> row >> col;
+	string str_num;
+	int num;
 	for (int i = 0; i < row; i++)
 	{
-		vector<int> temp;
-		cin >> num;
-		str_num = to_string(num);
-		for (int j = 0; j < col; j++)
+		vector<int>nums;
+		cin >> str_num;
+		for (int j = 0;j < col; j++)
 		{
-			temp.push_back(str_num[j]-'0');
+			nums.push_back(str_num[j] - '0');
 		}
-		miro.push_back(temp);
+		miro.push_back(nums);
 	}
 
-	cout << bfs(0, 0);
+	dfs(0, 0);
+
+	cout << answer;
 
 
 	return 0;
