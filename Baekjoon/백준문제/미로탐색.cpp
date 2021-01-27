32d22575a336814f4a57b77a423b9d63e57fcdb2
eunseon <kwon9348@naver.com>
2021-01-27 17:50:41 +0900

    bfs사용 - 성공

--- Baekjoon/백준문제/미로탐색.cpp
+++ Baekjoon/백준문제/미로탐색.cpp
@@ -1,61 +1,62 @@
-﻿//2178번
-//dfs사용
+﻿//2178
+//bfs사용
+//대표적인 bfs문제 -> 최단 거리를 찾을때 사용됨.
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <string>
 
 using namespace std;
 
-int col, row;
+int dx[] = { 0,1,0,-1 };
+int dy[] = { 1,0,-1,0 };
 bool visit[100][100] = { false, };
+int row, col;
 vector<vector<int> > miro;
-int dx[] = { 1,0,0,-1 };
-int dy[] = { 0,1,-1,0 };
-int cnt = 1, answer;
 
-void dfs(int x, int y) {
-	if (visit[x][y])
-		return;
-	if (x == row - 1 && y == col - 1)
-		answer = cnt;
-	visit[x][y] = true;
-	cnt++;
-	int next_x, next_y;
-	bool flag = false;
-	for (int i = 0; i < 4; i++)
-	{
-		next_x = x + dx[i];
-		next_y = y + dy[i];
-		if (next_x >= 0 && next_x < row && next_y >= 0 && next_y < col) {
-			if (miro[next_x][next_y] == 1 && !visit[next_x][next_y]) {
-				flag = true;
-				dfs(next_x, next_y);
+int bfs(int a, int b,int cnt) {
+	int x, y, next_x, next_y;
+	queue<pair<pair<int, int>, int> > q;
+	q.push(make_pair(make_pair(a, b), cnt));
+	visit[a][b] = true;
+	while (!q.empty()) {
+		x = q.front().first.first;
+		y = q.front().first.second;
+		cnt = q.front().second ;
+		q.pop();
+		if (x == row - 1 && y == col - 1)
+			return cnt;
+		for (int i = 0; i < 4; i++)
+		{
+			next_x = x + dx[i];
+			next_y = y + dy[i];
+			if (next_x < row&&next_x >= 0 && next_y < col&&next_y >= 0 && !visit[next_x][next_y]) {
+				visit[next_x][next_y] = true;
+				if(miro[next_x][next_y]==1)
+					q.push(make_pair(make_pair(next_x, next_y), cnt+1));
 			}
 		}
+
 	}
-	if (!flag) {
-		cnt--;
-	}
+	return cnt;
+
 }
 
 int main() {
 	cin >> row >> col;
-	string str_num;
-	int num;
+	string s_num;
 	for (int i = 0; i < row; i++)
 	{
 		vector<int>nums;
-		cin >> str_num;
-		for (int j = 0;j < col; j++)
+		cin >> s_num;
+		for (int j = 0; j < col; j++)
 		{
-			nums.push_back(str_num[j] - '0');
+			nums.push_back(s_num[j] - '0');
 		}
 		miro.push_back(nums);
 	}
 
-	dfs(0, 0);
-
-	cout << answer;
+	cout << bfs(0, 0,1);
 
 
 	return 0;
