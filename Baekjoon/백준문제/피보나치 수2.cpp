bc3038c4f0d6b932072919339c599d85b6b00918
eunseon <kwon9348@naver.com>
2021-01-28 23:35:36 +0900

    dp사용- 성공

---
+++ Baekjoon/백준문제/피보나치 수2.cpp
@@ -0,0 +1,22 @@
+﻿//2748
+//dp이용
+
+#include <iostream>
+using namespace std;
+
+long long dp[91];
+
+
+int main() {
+	//변수의 크기!!
+	long long n;
+	cin >> n;
+	dp[0] = 0;
+	dp[1] = 1;
+	for (int i = 2; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[n];
+	return 0;
+}
