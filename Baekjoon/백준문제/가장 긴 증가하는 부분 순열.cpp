8fe48451b3904b351ae7f97d9e6f6599772d9c15
eunseon <kwon9348@naver.com>
2021-01-30 01:37:31 +0900

    힌트보고 성공

---
+++ Baekjoon/백준문제/가장 긴 증가하는 부분 순열.cpp
@@ -0,0 +1,41 @@
+//11053
+//dp»ç¿ë
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int num[1000];
+int dp[1000];
+
+int main() {
+
+	int n, temp, answer = 1;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> temp;
+		num[i] = temp;
+	}
+	
+	for (int i = 0; i < n; i++)
+	{
+		dp[i] = 1;
+		for (int j = 0; j < i; j++)
+		{
+			if (num[j] < num[i]) {
+				dp[i] = max(dp[j] + 1, dp[i]);
+			}
+		}
+	}
+
+	cout <<*max_element(dp,dp+1000);
+
+	return 0;
+}
+
+/*
+9
+10 20 15 20 30 60 20 40 50
+*/
