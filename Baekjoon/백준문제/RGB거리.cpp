192e0e781fcb7e622668ffb884bc5cd7fd8f217a
eunseon <kwon9348@naver.com>
2021-01-29 23:20:27 +0900

    다시 한번 더 풀어봄

--- Baekjoon/백준문제/RGB거리.cpp
+++ Baekjoon/백준문제/RGB거리.cpp
@@ -15,10 +15,8 @@ int cost[1001][3];
 int main() {
 	int n,temp;
 	cin >> n;
-	house[0][0] = 0;
-	house[0][1] = 0;
-	house[0][2] = 0;
-	for (int i = 1; i <= n; i++)
+	;
+	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < 3; j++)
 		{
@@ -26,14 +24,18 @@ int main() {
 			cost[i][j] = temp;
 		}
 	}
-	for (int i = 1; i <= n; i++)
+	house[0][0] = cost[0][0];
+	house[0][1] = cost[0][1];
+	house[0][2] = cost[0][2];
+
+	for (int i = 1; i < n; i++)
 	{
 		house[i][0] = min(house[i - 1][1], house[i - 1][2]) + cost[i][0];
 		house[i][1] = min(house[i - 1][0], house[i - 1][2]) + cost[i][1];
 		house[i][2] = min(house[i - 1][0], house[i - 1][1]) + cost[i][2];
 	}
 
-	cout << min(house[n][0], min(house[n][1], house[n][2]));
+	cout << min(house[n-1][0], min(house[n-1][1], house[n-1][2]));
 
 
 
