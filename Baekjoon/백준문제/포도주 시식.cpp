c8bf86e0d7ad0ee5ed9842d6c80e6f4e7cfcf546
eunseon <kwon9348@naver.com>
2021-01-29 23:54:07 +0900

    계단 오르길 방식으로 풂 - > 실패

---
+++ Baekjoon/백준문제/포도주 시식.cpp
@@ -0,0 +1,29 @@
+//2156
+//dp»ç¿ë
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+long long wine[10000];
+long long value[10000];
+
+int main() {
+	int n,temp;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> value[i];
+	}
+	wine[0] = value[0];
+	wine[1] = value[0] + value[1];
+	for (int i = 2; i < n; i++)
+	{
+		wine[i] = max(value[i] + wine[i - 2], value[i] + value[i - 1] + wine[i - 3]);
+	}
+	cout << *max_element(wine, wine + 10000);
+
+
+	return 0;
+}
