83dc0c923dbc8888b85f094cb7faf1474e88ecc7
eunseon <kwon9348@naver.com>
2021-01-29 23:53:46 +0900

    dp사용 - 성공

---
+++ Baekjoon/백준문제/이친수.cpp
@@ -0,0 +1,23 @@
+//2193¹ø
+//dp»ç¿ë
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[91];
+
+int main() {
+	int N;
+	cin >> N;
+	dp[1] = 1;
+	dp[2] = 1;
+	for (int i = 3; i <= N; i++)
+	{
+		dp[i] = dp[i - 2] + dp[i - 1];
+	}
+	cout << dp[N];
+
+	return 0;
+}
+
