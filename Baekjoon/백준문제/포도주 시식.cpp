8da643e84bc791f4f36d3f5d24fde81d683fd15c
eunseon <kwon9348@naver.com>
2021-01-30 00:05:04 +0900

    dp이용 -> 성공 계단오르기와 다르게 각 값들을 합할때  제한개수가 없었다.

--- Baekjoon/백준문제/포도주 시식.cpp
+++ Baekjoon/백준문제/포도주 시식.cpp
@@ -9,6 +9,17 @@ using namespace std;
 long long wine[10000];
 long long value[10000];
 
+int max_wine(long long arr[], int size) {
+	int answer= 0 ;
+	for (int i = 0; i <= size; i++)
+	{
+		if (answer < arr[i])
+			answer = arr[i];
+	}
+	return answer;
+}
+
+
 int main() {
 	int n,temp;
 	cin >> n;
@@ -20,7 +31,7 @@ int main() {
 	wine[1] = value[0] + value[1];
 	for (int i = 2; i < n; i++)
 	{
-		wine[i] = max(value[i] + wine[i - 2], value[i] + value[i - 1] + wine[i - 3]);
+		wine[i] = max(value[i] + max_wine(wine, i-2), value[i] + value[i - 1] + max_wine(wine, i-3));
 	}
 	cout << *max_element(wine, wine + 10000);
 
