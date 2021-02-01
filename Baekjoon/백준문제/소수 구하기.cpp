0ed846bee67847d617855aa2a7b366118f4d00f7
eunseon <kwon9348@naver.com>
2021-02-02 02:51:57 +0900

    에라토스테네스의 체 사용->성공

---
+++ Baekjoon/백준문제/소수 구하기.cpp
@@ -0,0 +1,26 @@
+//1929
+//에라토스테네스의 체 사용
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+	int first, last;
+	cin >> first >> last;
+	int *arr = new int[last + 1];
+	for (int i = 2; i <= last; i++)
+	{
+		for (int j = i+i; j <= last; j+=i)
+		{
+			arr[j] = 1;
+		}
+	}
+	for (int i =first; i <= last; i++)
+	{
+		if (arr[i] != 1) {
+			cout << i << "\n";
+		}
+	}
+
+	return 0;
+}
