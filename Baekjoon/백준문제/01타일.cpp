64f8adf797d4c52219feb624feee6f461c01ffcf
eunseon <kwon9348@naver.com>
2021-02-13 23:59:16 +0900

    dp사용 -실패

---
+++ Baekjoon/백준문제/01타일.cpp
@@ -0,0 +1,19 @@
+//1904
+//dp
+#include <iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	int *dp = new int[n + 1];
+	dp[1] = 1;
+	dp[2] = 2;
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	cout << dp[n];
+	return 0;
+}
