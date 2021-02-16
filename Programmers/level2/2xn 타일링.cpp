4dd630d7290e4c0e4ebf2d3b70a6494e9b690cc6
eunseon <kwon9348@naver.com>
2021-02-16 22:30:26 +0900

    dp사용 -성공

---
+++ Programmers/level2/2xn 타일링.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int n ) {
+	int answer = 0;
+	int *dp = new int[n+1];
+	dp[1] = 1;
+	dp[2] = 2;
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2]; 
+	}
+
+	answer = dp[n] % 1000000007;
+	return answer;
+}
+
+int main() {
+	solution(9);
+
+}
