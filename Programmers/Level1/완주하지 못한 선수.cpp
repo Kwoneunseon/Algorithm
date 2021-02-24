057af27be4a24a34491c1aaad0b5bba6d0e8f6e3
eunseon <kwon9348@naver.com>
2021-02-25 00:04:40 +0900

    성공

---
+++ Programmers/Level1/완주하지 못한 선수.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string solution(vector<string>participant, vector<string> completion) {
+	string answer = "";
+	sort(participant.begin(), participant.end());
+	sort(completion.begin(), completion.end());
+	for (int i = 0; i < participant.size()-1; i++)
+	{
+		if (participant[i] != completion[i]) {
+			answer = participant[i];
+			break;
+		}
+	}
+	answer = participant[participant.size() - 1];
+	return answer;
+}
+
+int main() {
+	solution({ "mis","stan","mis","ana"}, { "stan","mis","ana" });
+		
+}
