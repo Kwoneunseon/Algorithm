881c5689e617ae72135060f9f6dad0e3f0723a63
eunseon <kwon9348@naver.com>
2021-02-27 11:16:53 +0900

    복습

--- Programmers/level2/N으로 표현.cpp
+++ Programmers/level2/N으로 표현.cpp
@@ -2,40 +2,38 @@
 //dp
 //º¹½À - 2021.02.27 (https://dvpzeekke.tistory.com/73)
 
-#include <string>
 #include <vector>
+#include <string>
+#include <algorithm>
 #define MAX 9
-
-using namespace std;
 
 int minDepth = MAX;
 
 void dfs(int N, int number, int depth, int num) {
-	if (depth >= MAX) {
+	if (depth >= MAX)
 		return;
-	}
+		
 
 	if (num == number) {
 		minDepth = min(depth, minDepth);
 	}
 
 	int operand = 0;
-	for (int i = 1; i <= MAX; i++) {
+	for (int i = 0; i <= MAX ; i++)
+	{
 		operand = operand * 10 + N;
 
 		dfs(N, number, depth + i, num + operand);
-		dfs(N, number, depth + i, num - operand);
-
+		dfs(N, number, depth + i, num- operand);
 		if (num != 0) {
-			dfs(N, number, depth + i, num * operand);
-			dfs(N, number, depth + i, num / operand);
+			dfs(N, number, depth + i, num*operand);
+			dfs(N, number, depth + i, num/ operand);
 		}
 	}
 }
 
 int solution(int N, int number) {
 	int answer = 0;
-
 	dfs(N, number, 0, 0);
 	answer = ((minDepth >= MAX) ? -1 : minDepth);
 
