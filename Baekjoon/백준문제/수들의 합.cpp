c926b7f064f6caa08c7c24e8e7c9ed5e8d54fc75
eunseon <kwon9348@naver.com>
2021-03-10 22:02:58 +0900

    질문보고 해결

--- Baekjoon/백준문제/수들의 합.cpp
+++ Baekjoon/백준문제/수들의 합.cpp
@@ -6,34 +6,16 @@
 
 using namespace std;
 
-long long check(int mid) {
-	long long sum = 0;
-	for (int i = 1; i <= mid; i++)
+int main() {
+	int count = 0;
+	long long S, sum =0;
+	cin >> S;
+	for (int i = 1 ;sum <= S ; i++)
 	{
+		count++;
 		sum += i;
 	}
-	return sum;
-}
-
-int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-	int S, N,answer = 0;
-	cin >> S;
-	int low = 1, high = sqrt(MAX);
-	while (low <= high) {
-		int mid = (low + high) / 2;
-		if (check(mid) <= S) {
-			answer = max(answer, mid);
-			low = mid + 1;
-		}
-		else {
-			high = mid - 1;
-		}
-
-	}
-	cout << answer;
-
+	cout << count-1;
 
 	return 0;
 }
