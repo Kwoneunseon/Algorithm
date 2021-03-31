85563fa29468e5d3e1c923b384ca054baf4aea19
eunseon <kwon9348@naver.com>
2021-03-31 11:55:05 +0900

    성공 - Union-Find사용

--- Baekjoon/백준문제/공항.cpp
+++ Baekjoon/백준문제/공항.cpp
@@ -1,38 +1,52 @@
 #include <iostream>
-#include <cstring>
 #include <vector>
 
 using namespace std;
 
-int airport[100002];
+int getRootgate(vector<int>& gate, int a) {
+	if (gate[a] == a)return a;
+	return gate[a] = getRootgate(gate,gate[a]);
+}
+
+void Union_gate(vector<int>& gate, int a, int b) {
+	//a = getRootgate(gate, a);
+	b = getRootgate(gate, b);
+	gate[a] = b;
+}
+
+
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	int G, P, g, count = 0;
-	bool flag = false;
+
+	int G, P, g,count = 0;
+	bool flag = true;
 	cin >> G >> P;
-	memset(airport, 0, G + 2);
+	vector<int>plane(P+1);
+	vector<int>gate(G + 1);
+	for (int i = 0; i <= G; i++)
+	{
+		gate[i] = i;
+	}
+
 	for (int i = 0; i < P; i++)
 	{
-		cin >> g;
-		flag = false;
-		for (int j = g; j >= 1; j--)
-		{
-			if (airport[j] == 0) {
-				airport[j] = 1;
-				flag = true;
-				count++;
-				break;
-			}
+		cin >> plane[i];
+		int Rootgate = getRootgate(gate, plane[i]);
+
+		if (Rootgate != 0 && flag) {
+			Union_gate(gate, Rootgate, Rootgate - 1);
+			count++;
 		}
-		if (!flag)
-			break;
+		else {
+			flag = false;
+			continue;
+		}
 	}
 
 	cout << count;
-
 
 	return 0;
 }
