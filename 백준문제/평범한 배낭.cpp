16c79943865f485cb3cc844870adc9b227c76b37
eunseon <kwon9348@naver.com>
2021-06-21 22:46:20 +0900

    knapsack 알고리즘

--- 백준문제/평범한 배낭.cpp
+++ 백준문제/평범한 배낭.cpp
@@ -1,39 +1,37 @@
 //12865
 //dp - knapsack
-//º¹½À - 2021.03.13
-//2021.03.13
-//12865
 #include <iostream>
-#include <cstring>
 #include <algorithm>
 
 using namespace std;
 
-int main() {
-	int n, k;
-	int items[101][2];
-	int dp[101][100001] = { 0, };
-	cin >> n >> k;
-	for (int i = 0; i < n; i++)
-	{
-		cin >> items[i][0] >> items[i][1];
-	}
+int items[101][2];
+int dp[100][100001] = { 0, };
 
+int main() {
+	int n, k, w, v;
+	cin >> n >> k;
 	for (int i = 1; i <= n; i++)
 	{
-		for (int j = 1; j <=  k; j++)
+		cin >> w >> v;
+		items[i][0] = w; 
+		items[i][1] = v;
+	}
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= k; j++)
 		{
-			if (j > items[i][0]) {
+			if (j >= items[i][0]) {
 				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - items[i][0]] + items[i][1]);
 			}
 			else
-				dp[i][j] = dp[i - 1][j];
+				dp[i][j] = dp[i][j - 1];
 		}
 	}
 	cout << dp[n][k];
 
-
 	return 0;
+
 }
 
 /*
