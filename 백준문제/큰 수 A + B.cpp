6ccafecae0e17505571368ab29c18be3c4639cb8
eunseon <kwon9348@naver.com>
2021-07-21 00:19:51 +0900

    성공!

--- 백준문제/큰 수 A + B.cpp
+++ 백준문제/큰 수 A + B.cpp
@@ -7,31 +7,38 @@ using namespace std;
 //long long의 범위인 -2,147,483,648~2,147,483,647를 넘었기에
 //string으로 하나하나씩 계산
 int main() {
-	string a, b,answer="";
-	int a_size, b_size, max_size,temp=0;
+	string a, b, answer = "";
+	int a_size, b_size, max_size, temp = 0;
 
 	cin >> a >> b;
 	a_size = a.length();b_size = b.length();
 	max_size = max(a_size, b_size);
 	vector<int> va, vb(max_size);
 	bool flag = false;
-	for (int i = 0; i < max_size; i++)
+
+	if (a_size < b_size) {
+		for (int i = 0; i < b_size - a_size; i++)
+		{
+			a = '0' + a;
+		}
+	}
+	else {
+		for (int i = 0; i < a_size - b_size; i++)
+		{
+			b = '0' + b;
+		}
+	}
+
+
+	for (int i = max_size - 1; i >= 0; i--)
 	{
 		if (flag) {
-			temp =1;
-			flag = false;			
+			temp = 1;
+			flag = false;
 		}
-		if (i > a_size) {
-			temp = temp + (a[i] - '0');
 
-		}
-		else if(i > b_size){
-			temp = temp + (b[i] - '0');
-		}
-		else {
-			temp = temp + (a[i] - '0') + (b[i] - '0');
-	
-		}
+		temp = temp + (a[i] - '0') + (b[i] - '0');
+
 		if (temp > 9) {
 			flag = true;
 			temp = temp - 10;
@@ -39,6 +46,8 @@ int main() {
 		answer.push_back(temp + '0');
 		temp = 0;
 	}
+	if (flag)
+		answer.push_back('1');
 
 	reverse(answer.begin(), answer.end());
 	cout << answer;
