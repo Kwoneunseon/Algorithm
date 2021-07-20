f3d6c73f0a27bcefa58b123e47fcfff3b573bc25
eunseon <kwon9348@naver.com>
2021-07-20 23:41:15 +0900

    도전 - 실패..

--- 백준문제/컨베이어 벨트 위의 로봇.cpp
+++ 백준문제/컨베이어 벨트 위의 로봇.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -7,18 +8,24 @@ int main() {
 	int n, k;
 	int zeros = 0, cnt = 0;
 	cin >> n >> k;
-	vector<pair<int,bool> >con(2 * n);
-	for (int i = 0; i < 2*n; i++)
+	vector<pair<int, bool> >con(2 * n);
+	for (int i = 0; i < 2 * n; i++)
 	{
 		cin >> con[i].first;
 		con[i].second = false;
 	}
-	con[0].first--;con[0].second = true;//처음 상자올리기
-	pair<int, bool> temp1,temp2;
+	//처음 상자를 올릴 때.
+
+	con[0].first--;
+	con[0].second = true;
+	if (con[0].first == 0)
+		zeros++;
+
+	pair<int, bool> temp1, temp2;
 	while (zeros != k) {
 		//컨테이너를 먼저 옮긴다.
 		temp1 = con[0];
-		for (int i =1; i < 2*n; i++)
+		for (int i = 1; i < 2 * n; i++)
 		{
 			if (i + 1 == 2 * n) {
 				temp2 = con[i];
@@ -30,32 +37,36 @@ int main() {
 				temp2 = con[i];
 				con[i] = temp1;
 				temp1 = temp2;
-			}			
+			}
 		}
-		//상자 
+		//상자 올리기
 		if (con[0].first != 0) {
 			con[0].first--;
+			con[0].second = true;
 			if (con[0].first == 0)
 				zeros++;
-			con[0].second = true;
 		}
-		for (int i = 1; i < 2*n; i++)
+		for (int i = n-1; i >= 1; i--)
 		{
-			if (con[i].second = true) {
-				if (con[(i + 1)%(2*n)].second == false && con[(i + 1) % (2 * n)].first > 0) {
-					con[(i + 1) % (2 * n)].second = true;
-					con[(i + 1) % (2 * n)].first--;
-					if (con[(i + 1) % (2 * n)].first == 0)
-						zeros++;
-				}
-			}
-			if (i == n) {
+			if (i == n-1) { //박스 내리는 위치
 				con[i + 1].second = false;
+			}
+			else if(con[i].second == true) {
+				//옆 칸에 로봇이 있는지 확인하고 내구도 1이상일 때 움직이게 한다.
+				if (con[i + 1 ].second == false && con[i + 1].first != 0) {
+					con[i].second = false;
+					con[i + 1].second = true;
+					con[i + 1].first--;
+					if (con[i + 1].first == 0)
+						zeros++;
+					i++;
+				}
 			}
 		}
 		cnt++;
 
 	}
+	cout << cnt;
 
 	return 0;
 }
