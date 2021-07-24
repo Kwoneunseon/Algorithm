8965463018b44acda0ce37e69d5f9d8d684d119d
eunseon <kwon9348@naver.com>
2021-07-24 23:31:47 +0900

    이전 보다 짧은 코드

--- 백준문제/네 번째 점.cpp
+++ 백준문제/네 번째 점.cpp
@@ -3,38 +3,16 @@
 using namespace std;
 
 int main() {
-	int new_x, new_y;
-	int x[3] = { 0, }, y[3] = { 0, };
-	for (int i = 0; i < 3; i++)
-	{
-		cin >> new_x >> new_y;
-		x[i] = new_x;y[i] = new_y;
-		for (int j = 0; j < i; j++)
-		{
-			if (x[j] == new_x) {
-				x[j] = 0;
-				x[i] = 0;
-			}
-			if (y[j] == new_y) {
-				y[j] = 0;
-				y[i] = 0;
-			}
-		}
-	}
-	for (int i = 0; i < 3; i++)
-	{
-		if (x[i] != 0) {
-			cout << x[i]<<" ";
-			break;
-		}
-	}
-	for (int i = 0; i < 3; i++)
-	{
-		if (y[i] != 0) {
-			cout << y[i];
-			break;
-		}
-	}
+	int x1, x2, x3, y1, y2, y3;
+	cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3;
+	if (x1 == x2)cout << x3 << " ";
+	else if (x1 == x3) cout << x2 << " ";
+	else if (x2 == x3) cout << x1 << " ";
+
+	if (y1 == y2)cout << y3 << " ";
+	else if (y1 == y3) cout << y2 << " ";
+	else if (y2 == y3) cout << y1 << " ";
+
 
 	return 0;
 }
