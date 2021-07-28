463b3972b8afdd63f7bdf9bff67f25f628c8f9be
eunseon <kwon9348@naver.com>
2021-07-29 01:12:43 +0900

    map사용 - 성공

--- 백준문제/나는야 포켓몬 마스터 이다솜.cpp
+++ 백준문제/나는야 포켓몬 마스터 이다솜.cpp
@@ -2,35 +2,37 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <map>
 
 using namespace std;
 
-int main() {
-	ios_base::sync_with_stdio(false);
+int  main() {
+	//항상 써주자!
 	cin.tie(NULL);
-	int m, n,temp;
+	ios_base::sync_with_stdio(false);
+	//map[key]=value
+	map<string, int> pock_map;
+	int m, n;
 	string s;
 	cin >> m >> n;
-	vector<string>dic(m+1);
+	string *pock_arr = new string[m + 1];
 	for (int i = 1; i <= m; i++)
 	{
-		cin >> dic[i];
+		cin >> s;
+		pock_map.insert({ s,i });
+		pock_arr[i] = s;
 	}
 	for (int i = 0; i < n; i++)
 	{
 		cin >> s;
 		if (s[0] >= '0' && s[0] <= '9') {
-			temp = stoi(s);
-			if (temp >= 1 && temp < m) {
-				cout << dic[temp] << "\n";
-			}
+			int temp = stoi(s);
+			cout << pock_arr[temp]<<"\n";
 		}
 		else {
-			auto it = find(dic.begin(), dic.end(), s);
-			cout << it - dic.begin()<<"\n";
+			cout << pock_map[s]<<"\n";
 		}
 	}
-	
 
 	return 0;
 }
