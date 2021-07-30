254386409450718e5ee37aa158a98d726eec1a63
eunseon <kwon9348@naver.com>
2021-07-30 23:45:53 +0900

    binary search사용 - 성공 (참고해서 풂)

--- 백준문제/듣보잡.cpp
+++ 백준문제/듣보잡.cpp
@@ -1,42 +1,55 @@
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <algorithm>
 #include <string>
-//binary search
+//binary search 사용함.
+//참조 블로그 : https://jaimemin.tistory.com/672
 
 using namespace std;
 
+vector<string> no_listen, result;
+
+bool binary_search(int low, int high, string name) {
+	if (low > high)
+		return false;
+	else {
+		int mid = (low + high) / 2;
+
+		if (no_listen[mid] == name)
+			return true;
+		else if (no_listen[mid] > name)
+			return binary_search(low, mid - 1, name);
+		else
+			return binary_search(mid + 1, high, name);
+	}
+}
+
 int main() {
+	cin.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	string s;
 	int n, m;
 	cin >> n >> m;
-	vector<string> no_listen(n), no_see(m),answer;
+	//듣도 못한 사람 입력받음.
 	for (int i = 0; i < n; i++)
 	{
-		cin >> no_listen[i];
+		cin >> s;
+		no_listen.push_back(s);
 	}
-	for (int i = 0; i < m; i++)
-	{
-		cin >> no_see[i];
-	}
+	//이진탐색을 위해서 정렬
 	sort(no_listen.begin(), no_listen.end());
-	sort(no_see.begin(), no_see.end());
-	int i = 0, j = 0,flag= 0;
 	for (int i = 0; i < m; i++)
 	{
-		for (int j = 0; j < n; j++)
-		{
-			if (no_listen[j] == no_see[i]) {
-				answer.push_back(no_see[i]);
-				break;
-			}
-		}
+		cin >> s;
+		if (binary_search(0, (int)no_listen.size() - 1, s))
+			result.push_back(s);
 	}
-	cout << answer.size()<<"\n";
-	for (int i = 0; i < answer.size(); i++)
+	sort(result.begin(), result.end());
+	cout << result.size() << "\n";
+	for (int i = 0; i < result.size(); i++)
 	{
-		cout << answer[i]<<"\n";
+		cout << result[i] << "\n";
 	}
-
 	
 
 	return 0;
