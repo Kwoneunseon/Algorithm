1fac02b15d12642fce217551ba48922bf84ec751
eunseon <kwon9348@naver.com>
2021-07-31 23:53:13 +0900

    시간의 차이가 가장 적은순으로 오름차순 (실패) 끝나는 시간이 작은 순으로 오름차순(이게 정답, 찾아봄)

--- 백준문제/회의실 배정.cpp
+++ 백준문제/회의실 배정.cpp
@@ -14,22 +14,25 @@ bool compare(pair<int, pair<int,int>> a,pair<int, pair<int, int>> b) {
 int main() {
 	cin.tie(NULL);
 	ios_base::sync_with_stdio(false);
-	int n,first,last,dif,answer= 0;
+	int n,first,last,dif,answer= 0,max=-1;
 	cin >> n;
-	vector < pair<int, pair<int, int> >>times;
-	vector<int> time(n+1);
+	vector < pair<int, pair<int, int> >>schedule;
+
 	for (int i = 0; i < n; i++)
 	{
 		cin >> first >> last;
 		dif = last - first;
-		times.push_back({ dif,{first,last} });
+		if (last > max)
+			max = last;
+		schedule.push_back({ dif,{first,last} });
 	}
-	sort(times.begin(), times.end(),compare);
+	vector<int> time(max + 1);
+	sort(schedule.begin(), schedule.end(),compare);
 	bool check = false;
 	for (int i = 0; i < n; i++)
 	{
 		check = false;
-		for (int j = times[i].second.first+1; j < times[i].second.second; j++)
+		for (int j = schedule[i].second.first+1; j <= schedule[i].second.second; j++)
 		{
 			if (time[j] != 0) {
 				check = true;
@@ -40,7 +43,7 @@ int main() {
 			continue;
 		else {
 			answer++;
-			for (int j = times[i].second.first; j <= times[i].second.second; j++)
+			for (int j = schedule[i].second.first; j <= schedule[i].second.second; j++)
 			{
 				time[j] = 1;
 			}
