83126baed5a99a90dc197a77753ef5b7c7505adb
eunseon <kwon9348@naver.com>
2021-08-15 14:37:29 +0900

    찾아봐서 제출 안했어 나중에 풀어봐

---
+++ 백준문제/동전 1.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int money[101],dp[10001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> money[i];
+	}
+	dp[0] = 1;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = money[i]; j <= k; j++)
+		{
+			dp[j] = dp[j] + dp[j - money[i]];
+		}
+	}
+	cout << dp[k];
+
+	return 0;
+}
