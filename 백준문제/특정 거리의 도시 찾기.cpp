ad10a9bcb0747258856b1363a85cb8e19fc5b32c
eunseon <kwon9348@naver.com>
2021-08-31 19:34:21 +0900

    알고리즘 생각은 맞는데 86%에서 계속 틀림..

--- 백준문제/특정 거리의 도시 찾기.cpp
+++ 백준문제/특정 거리의 도시 찾기.cpp
@@ -1,43 +1,55 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <utility>
+#include <algorithm>
 using namespace std;
+
+bool visit[300001] = { false, };
+vector<int> map[300001];
 
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
-	
-	int N, M, K, X,start,destination;
+	cout.tie(NULL);
+	int N, M, K, X, start, destination;
 	cin >> N >> M >> K >> X;
-	vector<vector<int> >map(N + 1);
 	vector<int> answer;
 	for (int i = 0; i < M; i++)
 	{
 		cin >> start >> destination;
 		map[start].push_back(destination);
 	}
-	int *check = new int[N + 2];
-	queue<pair<int, int> > q;
+	queue<pair<int, int> >q;
+
 	q.push({ X,0 });
-	int point, cnt;
+	int point, count;
 	while (!q.empty()) {
 		point = q.front().first;
-		cnt = q.front().second;
+		count = q.front().second;
 		q.pop();
-		if (check[point] < 0) {
-			check[point] = cnt;
-			if (cnt == K)
-				answer.push_back(point);
+		if (count > K)
+			break;
+		else if (count == K)
+			answer.push_back(point);
+		else {
+			for (int i = 0; i < map[point].size(); i++)
+			{
+				if (!visit[map[point][i]]) {
+					q.push({ map[point][i],count + 1 });
+					visit[map[point][i]] = true;
+				}
+			}
+
 		}
 
-		for (int i = 0; i < map[point].size(); i++)
-		{
-			q.push({ map[point][i],cnt + 1 });
-		}
 	}
-	if (answer.size() == 0)
+
+	if (answer.size()==0) {
 		cout << "-1";
+	}
 	else {
+		sort(answer.begin(), answer.end());
 		for (int i = 0; i < answer.size(); i++)
 		{
 			cout << answer[i] << "\n";
