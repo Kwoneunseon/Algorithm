6846facfa374af21fa8f261334ea313e66740b65
eunseon <kwon9348@naver.com>
2021-09-17 23:56:29 +0900

    복습

--- 백준문제/미로탐색.cpp
+++ 백준문제/미로탐색.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 vector<vector<int> > map;
-bool visit[101][101] = { false, };
+bool visit_edge[101][101] = { false, };
 int n, m;
 
 int dx[] = { 1,-1,0,0 };
@@ -17,7 +17,7 @@ int bfs(int a,int b) {
 	int x, y, new_x, new_y;
 	queue<pair<pair<int, int> ,int>>q;
 	q.push(make_pair(make_pair(a, b), 1));
-	visit[a][b] = true;
+	visit_edge[a][b] = true;
 	while (!q.empty()) {
 		x = q.front().first.first;
 		y = q.front().first.second;
@@ -31,10 +31,10 @@ int bfs(int a,int b) {
 		{
 			new_x = x + dx[i];
 			new_y = y + dy[i];
-			if (new_x >= 0 && new_y >= 0 && new_x < n &&new_y < m && !visit[new_x][new_y]) {
+			if (new_x >= 0 && new_y >= 0 && new_x < n &&new_y < m && !visit_edge[new_x][new_y]) {
 				if (map[new_x][new_y] == 1) {
 					q.push(make_pair(make_pair(new_x, new_y), answer + 1));
-					visit[new_x][new_y] = true;
+					visit_edge[new_x][new_y] = true;
 				}
 			}
 
