b9348f912cabb3e811b3deb0f402c917a4650010
eunseon <kwon9348@naver.com>
2021-09-24 23:58:08 +0900

    예제 3에서 계속 잘못 출력됨.

--- 백준문제/아기 상어.cpp
+++ 백준문제/아기 상어.cpp
@@ -13,6 +13,8 @@ int N;
 
 
 int bfs(int start_x, int start_y,int fish) {
+	if (fish == 0)
+		return 0;
 	int x, y, new_x, new_y,shark,answer=0,count=0;
 	queue<pair<pair<int, int>, int> >q;
 	q.push({ {start_x,start_y},2 });
@@ -26,14 +28,13 @@ int bfs(int start_x, int start_y,int fish) {
 			new_x = x + dx[i];
 			new_y = y + dy[i];
 			if (new_x >= 0 && new_y >= 0 && new_x < N && new_y < N) {
-				if (map[new_x][new_y] != 0&&map[new_x][new_y] < shark) {
+				if (map[new_x][new_y] != 0 && map[new_x][new_y] < shark) {
 					count++;
 					shark += map[new_x][new_y];
 					map[new_x][new_y] = 0;
-					visit[new_x][new_y] = visit[x][y] + 1;
-					answer = max(answer, visit[new_x][new_y]);
+					visit[new_x][new_y] =  visit[x][y] + 1;
 					if (count == fish)
-						return answer;
+						return visit[new_x][new_y];
 					q.push({ {new_x,new_y},shark });
 					break;
 				}
@@ -48,8 +49,22 @@ int bfs(int start_x, int start_y,int fish) {
 	return answer;
 }
 
+int count_fishes(vector<int>v,int sum) {
+	int cnt_fish = v.size();
+	sort(v.begin(), v.end());
+
+	for (int i = v.size()-1; i >= 0; i--)
+	{
+		if ((sum - v[i]) < v[i])
+			cnt_fish--;
+		sum -= v[i];
+	}
+	return cnt_fish;
+}
+	
 int main() {
-	int x,y,fish=0;
+	int x,y,fish=2;
+	vector<int> fishes;
 	cin >> N;
 	for (int i = 0; i < N; i++)
 	{
@@ -59,11 +74,15 @@ int main() {
 			if (map[i][j] == 9) {
 				x = i;y = j;
 			}
-			else if (map[i][j] != 0)
-				fish++;
+			else if (map[i][j] != 0) {
+				fishes.push_back(map[i][j]);
+				fish += map[i][j];
+			}
 		}
 	}
 
+	fish = count_fishes(fishes, fish);
+	   
 	cout << bfs(x, y, fish);
 
 	return 0;
