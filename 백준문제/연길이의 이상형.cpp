bed2e81369999df8e15354f41a3adc09f886d1f6
eunseon <kwon9348@naver.com>
2021-09-28 00:39:20 +0900

    map을 사용 - 성공

---
+++ 백준문제/연길이의 이상형.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	map<char, char>mbti_map;
+
+	//map을 사용하여 반대의 값들의 Key와 value에 넣어준다.
+	mbti_map.insert({ 'E','I' });
+	mbti_map.insert({ 'I','E' });
+	mbti_map.insert({ 'S','N' });
+	mbti_map.insert({ 'N','S' });
+	mbti_map.insert({ 'F','T' });
+	mbti_map.insert({ 'T','F' });
+	mbti_map.insert({ 'P','J' });
+	mbti_map.insert({ 'J','P' });
+
+	string mbti, answer="";
+
+	cin >> mbti;
+	for (int i = 0; i < 4; i++)
+	{
+		answer.push_back(mbti_map[mbti[i]]);
+	}
+	cout << answer;
+	
+	return 0;
+}
