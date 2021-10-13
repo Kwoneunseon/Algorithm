8ed3bb9df9200425439ac9f84d24ee55ac835ad3
eunseon <kwon9348@naver.com>
2021-10-14 07:31:57 +0900

    분할정복 복습
    다시한번 풀어봐야할 듯

--- 백준문제/행렬제곱.cpp
+++ 백준문제/행렬제곱.cpp
@@ -2,70 +2,73 @@
 #include <vector>
 
 using namespace std;
+
 typedef long long ll;
-typedef vector < vector<ll> > Matrix;
+typedef vector<vector<ll> > matrix;
 
-Matrix operator  * (const Matrix &a, const Matrix &b) {
-	ll n = a.size();
-	Matrix re_mat(n,vector<ll>(n));
-	for (ll i = 0; i < n; i++)
-	{
-		for (ll j = 0; j < n; j++)
-		{
-			for (ll k = 0; k< n; k++)
-			{		
-				re_mat[i][j] += a[i][k] * b[k][j];
-			}
-			re_mat[i][j] %= 1000;
-		}
-	}
-	return re_mat;
-}
-
-Matrix power(Matrix a, ll n) {
+matrix operator * (const matrix &a, const matrix &b) {
 	ll size = a.size();
-	Matrix re_mat(size, vector<ll>(size));
+	matrix res(size, vector<ll>(size));
+
 	for (ll i = 0; i < size; i++)
 	{
-		re_mat[i][i] = 1;
+		for (ll j = 0; j < size; j++)
+		{
+			for (ll k = 0; k < size; k++)
+			{
+				res[i][j] += a[i][k] * b[k][j];
+			}
+			res[i][j] %= 1000;
+		}
 	}
+	return res;
+
+}
+
+matrix power(matrix a, ll n) {
+	ll size = a.size();
+	matrix res(size, vector<ll>(size));
+
+	for (ll i = 0; i < size; i++)
+	{
+		res[i][i] = 1;
+	}
+
 	while (n > 0) {
 		if (n % 2 == 1) {
-			re_mat = re_mat * a;
+			res = res * a;
 		}
 		n /= 2;
 		a = a * a;
 	}
-	return re_mat;
-}
-
-void print(const Matrix& matrix) {
-	ll size = matrix.size();
-	for (int i = 0; i < size; i++)
-	{
-		for (int j = 0; j < size; j++)
-		{
-			cout << matrix[i][j]<<" ";
-		}
-		cout << "\n";
-	}
+	return res;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
-	cout.tie(NULL);
-	ll n, k;
-	cin >> n >> k;
-	Matrix matrix(n, vector<ll>(n));
+	ios::sync_with_stdio(false);
+
+	ll n, b;
+	cin >> n >> b;
+	matrix a(n, vector<ll>(n));
 	for (ll i = 0; i < n; i++)
 	{
 		for (ll j = 0; j < n; j++)
 		{
-			cin >> matrix[i][j];
+			cin >> a[i][j];
 		}
 	}
-	print(power(matrix, k));
+	a= power(a, b);
+	for (ll i = 0; i < n; i++)
+	{
+		for (ll j = 0; j < n; j++)
+		{
+			cout << a[i][j]<<" ";
+		}
+		cout << "\n";
+	}
+	
+
 
 	return 0;
 }
