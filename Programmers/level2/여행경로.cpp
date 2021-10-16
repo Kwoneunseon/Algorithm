7e99968ab998cbe34897cad599ab95afe4726eab
eunseon <kwon9348@naver.com>
2021-10-16 23:39:40 +0900

    백 트래킹 알고리즘 복습

--- Programmers/level2/여행경로.cpp
+++ Programmers/level2/여행경로.cpp
@@ -3,73 +3,33 @@
 #include <vector>
 #include <string>
 #include <algorithm>
-#include <queue>
+
 using namespace std;
 
-vector<string>solution(vector<vector<string>>tickets) {
-	vector<string>answer;
-	vector<string>airport;
-	vector<vector<int>>tickets_int(10000);
-	int start_idx;
+bool dfs(string from, vector<vector<string>>tickets, vector<bool>&visit, vector<string>&answer, int cnt) {
+	answer.push_back(from);
+	if (cnt == tickets.size()) {
+		return true;
+	}
 	for (int i = 0; i < tickets.size(); i++)
 	{
-		for (int j = 0; j < 2; j++)
-		{
-			bool flag = true;
-			for (int k = 0; k < airport.size(); k++)
-			{
-				if (tickets[i][j] == airport[k])
-					flag = false;
-			}
-			if (flag) {
-				airport.push_back(tickets[i][j]);
-			}
+		if (tickets[i][0] == from && visit[i] == false) {
+			visit[i] = true;
+			bool success = dfs(tickets[i][1], tickets, visit, answer, cnt + 1);
+			if (success)
+				return true;
+			visit[i] = false;
 		}
-	} // 어떤 공항이 있는지 확인한다.
-	sort(airport.begin(), airport.end(),less<string>());
-	for (int i = 0; i < airport.size(); i++)
-	{
-		if (airport[i] == "ICN")
-			start_idx = i;
 	}
+	answer.pop_back();
+	return false;
+}
 
-	vector<vector<string>> check(10000);
-	for (int i = 0; i < tickets.size(); i++)
-	{
-		int x, y;
-		for (int j = 0; j < airport.size(); j++)
-		{
-			if (airport[j] == tickets[i][0]) {
-				x = j;
-				break;
-			}
-		}
-		for (int j = 0; j < airport.size(); j++)
-		{
-			if (airport[j] == tickets[i][1]) {
-				y = j;
-				break;
-			}
-		}
-		tickets_int[x].push_back(y);
-	} //숫자로 바꿔준다.	
-	for (int i = 0; i < tickets_int.size(); i++)
-	{
-		if(!tickets_int[i].empty())
-			sort(tickets_int[i].begin(), tickets_int[i].end());
-	}
-	int cur = start_idx;
-	answer.push_back(airport[cur]);
-	while (!tickets_int[cur].empty()) {
-		int next = tickets_int[cur][0];
-		tickets_int[cur].erase(tickets_int[cur].begin(), tickets_int[cur].begin() + 1);
-		answer.push_back(airport[next]);
-		cur = next;
-	}
-
-
-
-
+vector<string> solution(vector<vector<string>>tickets) {
+	vector<string> answer,temp;
+	vector<bool>visited(tickets.size(), false);
+	sort(tickets.begin(), tickets.end());
+	dfs("ICN", tickets, visited, answer, 0);
 	return answer;
 }
 
