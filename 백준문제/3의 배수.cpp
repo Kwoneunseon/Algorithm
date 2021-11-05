ce7090c2bc1c6ae22b49eb945b323f9ae36dceab
eunseon <kwon9348@naver.com>
2021-11-05 17:14:54 +0900

    간단 구현 문제

---
+++ 백준문제/3의 배수.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	
+	string number;
+	int count = 0;
+	cin >> number;
+	while (number.length()>1) {
+		int num_length = number.length();
+		count++;
+		int new_num=0;
+		for (int i = 0; i < num_length; i++)
+		{
+			new_num += (number[i] - '0');
+		}
+		number = to_string(new_num);
+	}
+	//변환 과정의 개수 출력
+	cout << count << "\n";
+	//3의 배수인지 아닌지 출력
+	if (stoi(number) % 3 == 0)
+		cout << "YES";
+	else
+		cout << "NO";
+
+
+
+	return 0;
+}
