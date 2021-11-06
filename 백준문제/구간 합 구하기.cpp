adf0e36b5b26d16fae247206bee567ad1f18afbc
eunseon <kwon9348@naver.com>
2021-11-06 23:44:25 +0900

    segment tree 시도 - 실패

--- 백준문제/구간 합 구하기.cpp
+++ 백준문제/구간 합 구하기.cpp
@@ -3,6 +3,34 @@
 
 using namespace std;
 
+vector<int> number;
+long long *sum_dp;
+
+int init(int start, int end, int node) {
+	if (start == end) return sum_dp[node] = number[start];
+	int mid = (start + end) / 2;
+
+	return sum_dp[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
+}
+
+int sum(int start, int end, int node, int left, int right) {
+	if (left > end || right < start) return 0;
+
+	if (left <= start && end <= right) return sum_dp[node];
+
+	int mid = (start + end) / 2;
+	return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
+}
+
+void update(int start, int end, int node, int index, int dif) {
+	if (index < start || index > end) return;
+
+	sum_dp[node] += dif;
+	if (start == end) return;
+	int mid = (start + end) / 2;
+	update(start, mid, node * 2, index, dif);
+	update(mid + 1, end, node * 2 + 1, index, dif);
+}
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -10,43 +38,30 @@ int main() {
 
 	cin >> num_size >> M >> K;
 
-	vector<int> number;
-	long long *sum_dp = new long long[num_size+2];
-	int *dif = new int[num_size + 2];
-	fill(dif, dif + num_size + 2, 0);
-	fill(sum_dp, sum_dp + num_size + 2, 0);
-
-	number.push_back(0);
+	sum_dp = new long long[4*num_size];
+	
 	for (int i = 1; i <= num_size; i++)
 	{
 		int temp;
 		cin >> temp;
 		number.push_back(temp);
-
-		if (i == 1)
-			sum_dp[i] = temp;
-		else
-			sum_dp[i] = sum_dp[i - 1] + temp;
 	}
+
+	init(0, num_size-1, 1);
+
+
 	int cmd , first,second;
 	for (int i = 0; i < M+K; i++)
 	{
 		cin >> cmd >> first >> second;
 		//first번째 수를 second로 바꾼다
 		if (cmd == 1) {
-			dif[first] = second;
+			second = second - number[first - 1];
+			update(0, num_size - 1, 1, first-1, second);
 		}
 		//first-second번째수까지의 합 구하기
 		else if (cmd == 2) {
-			long long answer = sum_dp[second] - sum_dp[first-1];
-			for (int j = first; j <= second; j++)
-			{
-				if (dif[j] != 0) {
-					answer += dif[j] - number[j];
-				}
-			}
-			cout << answer << "\n";
-
+			cout << sum(0, num_size, 1, first-1, second)<<"\n";
 		}
 	}
 
