0fd19caa6c7b74c4a333a9ae2727fa05673e5e54
eunseon <kwon9348@naver.com>
2022-07-17 22:50:22 +0900

    날짜 : 2022.07.17 작성자 : 권은선 변경 원인 : (백준)명령 프롬프트 코드 라인 축소 개선/개발 내역 :

--- 백준문제/명령 프롬프트.py
+++ 백준문제/명령 프롬프트.py
@@ -1,19 +1,10 @@
 N = int(input())
-strs = []
-for i in range(N) :
-    strs.append(input())
+str = list(input())
 
-if N == 1:
-    print(strs[0])
-else :
-    answer =[]
-    for idx in range(len(strs[0])):
-        #print(strs[0][idx])
-        for n_idx in range(N-1) :
-            if strs[n_idx][idx] != strs[n_idx+1][idx] :
-                answer.append('?')
-                break
-            if n_idx == N-2:
-                answer.append(strs[n_idx][idx])
+for _ in range(N-1):
+    str2 = list(input())
+    for idx in range(len(str)):
+        if str[idx] != str2[idx] :
+            str[idx] = '?'
 
-    print(''.join(answer))
+print(''.join(str))
