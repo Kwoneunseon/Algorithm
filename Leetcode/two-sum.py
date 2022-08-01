c2ec4a1393825c5effdb19c4c476165ca7779861
eunseon <kwon9348@naver.com>
2022-08-01 23:27:21 +0900

    날짜 : 2022.08.01 작성자 : 권은선 개선/개발 내역 : target - n이 나머지 리스트에 존재하는지 탐색하는 알고리즘 변경 원인 :Brute-Force가 너무 속도가 느려서

--- Leetcode/two-sum.py
+++ Leetcode/two-sum.py
@@ -2,7 +2,8 @@
 nums = [2,7,11,15]
 target =9
 
-for i in range(len(nums)):
-    for j in range(i+1,len(nums)):
-        if target == nums[i] + nums[j]:
-            print([i,j])
+for i, n in enumerate(nums):
+    complement = target - n
+
+    if complement in nums[i+1:]:
+        print (i, nums[i+1:].index(complement)+i+1 )
