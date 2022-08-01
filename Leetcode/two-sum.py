250b7d2b355a01ed08399faca7560c437eb558e0
eunseon <kwon9348@naver.com>
2022-08-01 23:33:42 +0900

    날짜 : 2022.08.01 작성자 : 권은선 개선/개발 내역 : 딕셔너리를 사용해 첫번째 수를 뺀 결과 키 조회 변경 원인 : 인데스와 값을 변경해 타겟에서 첫번째 값을 뺀 경우가 딕셔너리에 존재할 경우 인덱스 확인

--- Leetcode/two-sum.py
+++ Leetcode/two-sum.py
@@ -2,8 +2,10 @@
 nums = [2,7,11,15]
 target =9
 
-for i, n in enumerate(nums):
-    complement = target - n
+dic_num = {}
+for i, num in enumerate(nums):
+    dic_num[num]= i
 
-    if complement in nums[i+1:]:
-        print (i, nums[i+1:].index(complement)+i+1 )
+for i, num in enumerate(nums):
+    if target - num in dic_num and i != dic_num[target-num]:
+        print(i, dic_num[target-num])
